1b6a828d886b180aebcfb384f76e7ef4dbfecd88
DeaconRodda <deacon@sqglz.com>
2020-10-06 02:28:59 -0600

    card drawing function operational

--- scripts/regen-cards.js
+++ scripts/regen-cards.js
@@ -52,9 +52,13 @@ let shoeBox = [];
 	let rightLivelihood = new Card("Right Livelihood", 73, "Paying the bills while maintaining your ethical integrity can sometimes seem impossible, but there are a number of strategies for finding a healthy balance between the two.", "warriorship", "Adam Brock", ["question 1", "question 2", "question 3"]);
 	shoeBox.push(rightLivelihood);
 
+function getRandomInt(min, max) {
+  min = Math.ceil(0);
+  max = Math.floor(shoeBox.length);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
 
-
-const drawCard = function() {
+const cutCards = function() {
 
 	let deck = [];
 
@@ -97,8 +101,21 @@ const drawCard = function() {
 	}
 	stackDeck();
 
-	
-
 	return deck;
-
 };
+
+const drawCard = function() {
+
+	const deck = cutCards();
+
+	const selection = Math.floor(Math.random() * deck.length);
+	console.log(selection, deck[selection]);
+}
+
+
+
+
+
+
+
+
